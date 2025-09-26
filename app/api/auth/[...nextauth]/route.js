cdc6f46601ade902781e4fbed5584c6701937fec
Emad Poursina <emad.puursina@gmail.com>
2025-09-26 14:50:24 +0330

    Add next-auth credentials provider

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,46 @@
-import NextAuth from "next-auth"
+import { Prisma } from "@/lib/generated/prisma";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
-    providers: []
-})
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "test@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
 
-export { handler as GET, handler as POST }
+        // Check if email and password are provided  
+        if (!email || !password) {
+          return null;
+        }
+
+        // Check if user exists
+        const user = await Prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+        if (!user) {
+          return null;
+        }
+
+        // Check if password is correct
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return null;
+        }
+        return user;
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
