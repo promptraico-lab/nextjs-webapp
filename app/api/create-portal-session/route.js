57b44899ed36641f9b22f32d537aca20a44111f9
Emad Poursina <emad.puursina@gmail.com>
2025-10-09 22:22:39 +0330

    Stripe portal session handling

---
+++ app/api/create-portal-session/route.js
@@ -0,0 +1,42 @@
+// Set your secret key. Remember to switch to your live secret key in production.
+// See your keys here: https://dashboard.stripe.com/apikeys
+import { stripe } from "@/lib/stripe";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    // Parse the form data from the request
+    const formData = await req.formData();
+    const session_id = formData.get("session_id");
+
+    if (!session_id) {
+      return NextResponse.json(
+        { error: "Missing session_id" },
+        { status: 400 }
+      );
+    }
+
+    // Retrieve the checkout session to get the customer ID
+    const checkoutSession = await stripe.checkout.sessions.retrieve(session_id);
+    console.log("this is me: ", checkoutSession.customer);
+
+    if (!checkoutSession || !checkoutSession.customer) {
+      return NextResponse.json(
+        { error: "Invalid session or customer not found" },
+        { status: 400 }
+      );
+    }
+
+    // Create the billing portal session
+    const session = await stripe.billingPortal.sessions.create({
+      customer: checkoutSession.customer,
+      return_url: `${process.env.NEXT_PUBLIC_WEBSITE_URL}/admin/test`,
+    });
+    console.log("this is me 2: ", session.url);
+
+    // Redirect to the billing portal
+    return NextResponse.redirect(session.url);
+  } catch (err) {
+    return NextResponse.json({ error: err.message }, { status: 400 });
+  }
+}
