20c560e35091a15a25cf9d420d61dd9c3c9345da
Emad Poursina <emad.puursina@gmail.com>
2025-10-24 16:42:34 +0330

    install and setup react-oauth/google

--- components/providers.js
+++ components/providers.js
@@ -1,7 +1,14 @@
 "use client";
 
 import { Provider } from "jotai";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export const Providers = ({ children }) => {
-  return <Provider>{children}</Provider>;
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+
+  return (
+    <Provider>
+      <GoogleOAuthProvider clientId="192466123451-9dueae14nvv1irhpog6bimgp13ahdlnr.apps.googleusercontent.com">{children}</GoogleOAuthProvider>
+    </Provider>
+  );
 };
